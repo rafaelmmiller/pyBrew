64f82165a332a769556fd3c66b7361a627d792ed
Rafael <rafael.miller@usp.br>
2017-04-22 16:13:06 -0300

    alguns testes simples. Ainda aprendendo a brincar com o django

--- main_app/models.py
+++ main_app/models.py
@@ -3,3 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Treasure(models.Model):
+    name = models.CharField(max_length=100)
+    value = models.DecimalField(max_digits=100, decimal_places=2)
+
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Treasure
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    treasures = Treasure.objects.all()
+    return render(request, 'index.html', {'treasures':treasures})
